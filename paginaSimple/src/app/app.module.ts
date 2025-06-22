e4328295faebd7a87f06ec38a9b2d838f3361eb9
matiasbrandan <106163161+matiasbrandan@users.noreply.github.com>
2025-06-22 18:03:24 -0300

    tercer componente

--- paginaSimple/src/app/app.module.ts
+++ paginaSimple/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
